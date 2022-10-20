02a2187b40abf9d51a447f46626f6378dcf84bed
Chenjia Li <cligrenet@gmail.com>
2022-10-20 16:28:27 +0200

    Finetune landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import { GoPrimitiveDot } from 'react-icons/go';
+import { BsChevronDown } from 'react-icons/bs';
 import styles from '../../styles/Landing.module.css';
 
 const Landing = () => {
 	return (
-		<div>
-			<div>CHENJIA</div>
-			<div>
-				WEB DEVELOPER <GoPrimitiveDot />
-				RELIABLE <GoPrimitiveDot />
-				RESILIENT <GoPrimitiveDot />
-				TRANSPARENT <GoPrimitiveDot />
-				CLIENT AND DETIAL ORIENTED <GoPrimitiveDot />
+		<div className={styles.main}>
+			<span className={styles.title}>CHENJIA</span>
+			<div className={styles.banner}>
+				<p>
+					<span className={styles.desp}>
+						<span>
+							<GoPrimitiveDot />
+						</span>{' '}
+						WEB DEVELOPER
+					</span>
+					<span className={styles.desp}>
+						<span>
+							<GoPrimitiveDot />
+						</span>{' '}
+						RELIABLE COLLABORATOR
+					</span>
+					<span className={styles.desp}>
+						<span>
+							<GoPrimitiveDot />
+						</span>{' '}
+						PROBLEM SOLVER
+					</span>
+					<span className={styles.desp}>
+						<span>
+							<GoPrimitiveDot />
+						</span>{' '}
+						DETIAL FREAK
+					</span>
+				</p>
 			</div>
-			<div>LI-GRENET</div>
+			<span className={styles.title}>LI-GRENET</span>
+			<div className={styles.down}>
+				<span>
+					<BsChevronDown />
+				</span>
+			</div>
 		</div>
 	);
 };
