a6238d3789af124cbddabde35db10db2a69a6262
Chenjia Li <cligrenet@gmail.com>
2022-10-20 17:30:00 +0200

    Set up basic footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import Image from 'next/image';
 import styles from '../../styles/Footer.module.css';
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
 		<footer className={styles.footer}>
-			<div>
-				<FaLinkedinIn />
-				<FaGithub />
-			</div>
-
-			<div>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					&copy; Chenjia LI-GRENET 2022
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
+			<div className={styles.social}>
+				<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chenjialigrenet/">
+					<FaLinkedinIn size={30} />
+				</a>
+				<a target="_blank" rel="noopener noreferrer" href="https://github.com/cligrenet">
+					<FaGithub size={30} />
 				</a>
 			</div>
+
+			<div>&copy; Chenjia LI-GRENET 2022</div>
 		</footer>
 	);
 };
