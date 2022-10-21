49058f38b5a8bfbcd5d4d78885e040fd99f0b700
Chenjia Li <cligrenet@gmail.com>
2022-10-21 11:42:07 +0200

    Import link from next

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../../styles/Footer.module.css';
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
@@ -6,12 +7,16 @@ const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.social}>
-				<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chenjialigrenet/">
-					<FaLinkedinIn size={30} />
-				</a>
-				<a target="_blank" rel="noopener noreferrer" href="https://github.com/cligrenet">
-					<FaGithub size={30} />
-				</a>
+				<Link href="https://www.linkedin.com/in/chenjialigrenet/">
+					<a target="_blank" rel="noopener noreferrer">
+						<FaLinkedinIn size={30} />
+					</a>
+				</Link>
+				<Link href="https://github.com/cligrenet">
+					<a target="_blank" rel="noopener noreferrer">
+						<FaGithub size={30} />
+					</a>
+				</Link>
 			</div>
 
 			<div>&copy; Chenjia LI-GRENET 2022</div>
