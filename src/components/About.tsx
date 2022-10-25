2df8864d9ef11196514e0e09ae656adc8fc04f8b
Chenjia Li <cligrenet@gmail.com>
2022-10-25 11:39:46 +0200

    Polish about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,22 @@
 import React, { useRef, useEffect, useState } from 'react';
+import {
+	SiHtml5,
+	SiCss3,
+	SiJavascript,
+	SiTypescript,
+	SiReact,
+	SiNextdotjs,
+	SiNodedotjs,
+	SiExpress,
+	SiMongodb,
+	SiPostgresql,
+	SiGit,
+	SiGithub,
+	SiBootstrap,
+	SiTailwindcss,
+	SiPython,
+	SiSqlite,
+} from 'react-icons/si';
 import styles from '../../styles/About.module.css';
 
 type Props = {
@@ -20,27 +38,42 @@ const About = (props: Props) => {
 				ABOUT
 			</span>
 			{isClicked && (
-				<p className={styles.desp}>
-					Hello, I am Chenjia, a web developer.
-					<br />
-					Being recognised as a reliable contributor by various companies/clients around the world.
-					<br />
-					I am prepared to apply my communication, detail-oriented and problem solving skills in my new
-					missions.
-					<br />
-					After close to a year of honing my programming chops, both in an intense Bootcamp as well as
-					consistent self-study, I am ready and eager to join a concret project to put my skills to use!
-					<br />
-					I am most comfortable with front-end development and I would like to focus my effort there for now,
-					tho a bit of back-end work here and there is welcome to keep sharpening my overall understanding of
-					the web stack.
-					<br />
-					Here is my current tool box: JavaScript(ES6), TypeScript, HTML5, CSS3, ReactJS, Bootstrap,
-					TailwindCSS, NodeJS, ExpressJS, MongoDB, Python3, SQLite, PostgreSQL, Git, Github, Axios, REST
-					API...
-					<br />
-					Let me help you build your project.
-				</p>
+				<div className={styles.desp}>
+					<div className={styles.circle_container}>
+						<div className={styles.circle}>
+							<p>Hello, I am Chenjia, a web developer.</p>
+
+							<p>Being recognised as a reliable contributor by various clients around the world.</p>
+
+							<p>
+								I am most comfortable with front-end development and I would like to focus my effort
+								there for now, tho a bit of back-end work here and there is welcome to keep sharpening
+								my overall understanding of the web stack.
+							</p>
+
+							<p>Let me help you build your project.</p>
+						</div>
+					</div>
+
+					<div className={styles.icons}>
+						<SiHtml5 />
+						<SiCss3 />
+						<SiJavascript />
+						<SiTypescript />
+						<SiReact />
+						<SiNextdotjs />
+						<SiNodedotjs />
+						<SiExpress />
+						<SiMongodb />
+						<SiPostgresql />
+						<SiGit />
+						<SiGithub />
+						<SiBootstrap />
+						<SiTailwindcss />
+						<SiPython />
+						<SiSqlite />
+					</div>
+				</div>
 			)}
 		</div>
 	);
