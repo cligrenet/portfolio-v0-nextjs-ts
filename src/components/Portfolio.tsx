65e1ebc071dc39b6dbfd5ac108f193c3c67111de
Chenjia Li <cligrenet@gmail.com>
2022-10-25 16:19:56 +0200

    Add photos to portfolio section

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+import { motion, useScroll } from 'framer-motion';
 import styles from '../../styles/Portfolio.module.css';
 
 const Portfolio = () => {
+	const ref = useRef(null);
+	const { scrollXProgress } = useScroll({ container: ref });
 	const [isClicked, setIsClicked] = useState(false);
 
 	return (
@@ -9,7 +13,31 @@ const Portfolio = () => {
 			<span className={styles.title} onClick={() => setIsClicked(!isClicked)}>
 				PORTFOLIO
 			</span>
-			{isClicked && <div className={styles.desp}>projects</div>}
+			{isClicked && (
+				<div className={styles.desp}>
+					<ul ref={ref}>
+						<li>
+							<p>Meal Planner</p>
+							<Image src="https://i.imgur.com/jzaecf3.png" alt="helpdesk" width={200} height={230} />
+						</li>
+						<li>
+							<p>Helpdesk Ticketing</p>
+							<Image src="https://i.imgur.com/AM5q406.gif" alt="helpdesk" width={200} height={200} />
+						</li>
+						<li>
+							<p>Expense Tracker</p>
+							<Image src="https://i.imgur.com/36OnkAE.png" alt="helpdesk" width={170} height={250} />
+						</li>
+						<li>
+							<p>Toy Shop</p>
+							<Image src="https://i.imgur.com/6YMp5tq.png" alt="helpdesk" width={200} height={270} />
+						</li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 };
